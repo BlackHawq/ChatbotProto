56763df8e767b3644f2dd14a13b349e7cff179fa
BlackHawq <haq.hammud@gmail.com>
2025-07-16 15:05:49 -0700

    phase two of prototype Added second branch of decisions Some cleanup on input handling Comments added for clarity

--- main.py
+++ main.py
@@ -4,15 +4,47 @@
 def yesNoCheck(checkMe):
   # Assuming the person writes out a sentence or some phrase that contains yes or no 
    if checkMe and len(checkMe) < 500:
+      checkMe = checkMe.strip()
       checkMe = checkMe.lower()
       if "yes" in checkMe or "oui" in checkMe or "true" in checkMe or "affirmative" in checkMe or "si" in checkMe:
           return True
       elif "no" in checkMe or "non" in checkMe or "false" in checkMe or "negative" in checkMe or "nyet" in checkMe:
           print("No worries then! I hope you have a wonderful day!")
           return False
+      elif len(checkMe) == 1:
+         if checkMe == "y":
+            print("Kinda getting a yes vibe from your response")
+            return True
+         elif checkMe == "n":
+            print("Kind of got a no vibe from your response")
+            print("No worries then! I hope you have a wonderful day!")
+            return False      
       else:
-          print("Hmm I'm not sure I understood that can we try that again? ")
+          print("Hmm I'm not sure I understood that can we try that again?")
           return False
+
+def optionChecker(checkMe):
+  # Assuming the person writes out a sentence or some phrase that contains yes or no 
+   if checkMe and len(checkMe) < 500:
+      #Reduce variability by removing spaces and changing to lower case
+      checkMe = checkMe.strip() 
+      checkMe = checkMe.lower()
+      if "optionA" in checkMe or "option a" in checkMe or "name" in checkMe or "info" in checkMe:
+          return 0
+      elif "optionb" in checkMe or "option b" in checkMe or "address" in checkMe or "shipping" in checkMe:
+          return 1
+      elif "optionc" in checkMe or "option c" in checkMe or "order" in checkMe or "number" in checkMe:
+          return 2
+      elif checkMe == "a":
+         return 0
+      elif checkMe == "b":
+         return 1
+      elif checkMe == "c":
+         return 2
+      else:
+         print("I don't think I quite understood that...")
+         return 3
+
 
 def main():
     print("""/
@@ -45,25 +77,51 @@ def main():
 ⠀⠀⠀⠀⠀⠀⠀⠈⣿⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣿⡁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
 ⠀⠀⠀⠀⠀⠀⠀⢰⣿⠛⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⣇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
           """)
-    print("Hello you have reached the Aperture Labs Package Acquisition Computer Assistant")
+    print("Hello you have reached the Aperture Labs Package Acquisition Computer Assistant or A.L.P.A.C.A")
     print("I am designed to help you locate any packages that may be missing")
+    # Flag for controlling loop termination
     operational = True
+    # Counters for keeping track of failures in the individual processes
     choiceAErrorCounter = 0
     while operational:
+      # Choice A
+      # Initial Pass through should have unique dialog this will have additional conditions to account for repeated loop
       if choiceAErrorCounter == 0:
         print("To start off did you have a package you were looking for?")
         choiceA = input("Please respond with a yes or no: ")
+      # Successful dialog
       if yesNoCheck(choiceA):
-          print("I'm sorry our delivery service has you worried (╥﹏╥) Please allow me to assist you!")
+          print("\nI'm sorry our delivery service has you worried (╥﹏╥) Please allow me to assist you!")
+      # Successfully failed dialog
       elif choiceAErrorCounter <2:
           choiceAErrorCounter += 1
           print("Here, I'll let you have another go at it!")
           choiceA = input("You're sure that there's no package we can help you keep track of?(Yes/No): ")
+      # Maliciously or poorly accounted for failure state
       elif choiceAErrorCounter == 2:
          print("I'm sorry we weren't able to assist you please have a great day!")
          operational = False
+         break
       
-          
+      # Choice B method of determining how to find package status
+      print("So I know of three ways to find out where your missing package may be ")
+      print("Option A: Look it up by Name and information")
+      print("Option B: Find the order based on the address you wanted it shipped to")
+      print("Option C: If you have the order number we can try to look it")
+      choiceB = input("So what option would you like to go with A, B or C?: ")
+      choiceBOutPutter = optionChecker(choiceB)
+      if choiceBOutPutter == 0:
+        print("Ah you've selected option A the name and info option eh? Very well let's get down to business!")
+      elif choiceBOutPutter == 1:
+         print("Ooh going with Option B the address checker? An excellent choice!")
+      elif choiceBOutPutter == 2:
+         print("Yay option C! Lets get the order number ready for checking!")
+      else:
+         print("How strange it appears your option selection has opened a rip in the spacetime contiunum and taken you back to the previous choice!")
+         choiceBError = 0
+         while choiceBError <2 and choiceBOutPutter > 2:
+            choiceBOutPutter
+
 
 if __name__ == "__main__":
     main()
